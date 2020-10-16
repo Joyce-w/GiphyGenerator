897f8f77dda0ccc46134599b90789f29e8bc3736
Joyce <hellojoycewong@gmail.com>
2020-10-16 13:43:21 -0700

    functioning searchGif and populateGifs

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+    
 // Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
  
 $("#searchForm").on("submit", async function handleSearch(evt) {
@@ -6,25 +7,30 @@ $("#searchForm").on("submit", async function handleSearch(evt) {
     let searchVal = $("#searchTerm").val();
 
     console.log(searchVal)
-
     searchGif(searchVal)
+
+    let results = await searchGif(searchVal);
+
+    populateGifs(results)
 });
 
 // Once the Giphy API has responded with data, append the GIF to the page
 async function searchGif(searchVal) {
     let searchResult = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=uWKJbVAOQOeeTNzq9vaX7AzHfW8ISmYK&q=${searchVal}&limit=25&offset=0&rating=g&lang=en`)
 
-    
     let resultsLength = searchResult.data.data.length;
-
     let randomNum = Math.floor(Math.random()* (resultsLength + 1)) 
-    console.log(randomNum)
 
-    let results = searchResult.data.data[randomNum];
+    let results = searchResult.data.data[randomNum]
 
-    console.log(results.url)
+    return results.images.original.url
+   
 }
 
+ function populateGifs(results) {
+    $('.populatedGifs').append(`<img src=${results}/>`)
+
+}
 
 
 // Allow the user to search for as many GIFs as they would like and keep appending them to the page
